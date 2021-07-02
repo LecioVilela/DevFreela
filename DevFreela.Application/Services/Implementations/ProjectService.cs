ccf954ef5a580571e081ea0c3a49f41423b38630
LecioVilela <lecio.vilela@yahoo.com>
2021-07-02 14:00:06 -0300

    update project

---
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -0,0 +1,54 @@
+﻿using DevFreela.Application.InputModels;
+using DevFreela.Application.Services.Interfaces;
+using DevFreela.Application.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DevFreela.Application.Services.Implementations
+{
+    public class ProjectService : IProjectService
+    {
+        public int Create(NewProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void CreateComment(CreateCommentInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Delete(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Finish(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public List<ProjectViewModel> GetAll(string query)
+        {
+            throw new NotImplementedException();
+        }
+
+        public ProjectDetailsViewModel GetById(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Start(int id)
+        {
+            throw new NotImplementedException();
+        }
+
+        public void Update(UpdateProjectInputModel inputModel)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
