1451cb8ab861264e73ad262168bac00a9176edfa
LecioVilela <lecio.vilela@yahoo.com>
2021-08-06 17:26:29 -0300

    update project

---
+++ DevFreela.UnitTests/Core/Entities/ProjectTests.cs
@@ -0,0 +1,34 @@
+﻿using DevFreela.Core.Entities;
+using DevFreela.Core.Enums;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+
+namespace DevFreela.UnitTests.Core.Entities
+{
+    public class ProjectTests
+    {
+        [Fact]
+        public void TestIfProjectStartWorks()
+        {
+            var project = new Project("Nome De Teste", "Descricao de Teste", 1, 2, 10000);
+
+            Assert.Equal(ProjectStatusEnum.Created, project.Status);
+            Assert.Null(project.StartedAt);
+
+            Assert.NotNull(project.Title);
+            Assert.NotEmpty(project.Title);
+
+            Assert.NotNull(project.Description);
+            Assert.NotEmpty(project.Description);
+
+            project.Start();
+
+            Assert.Equal(ProjectStatusEnum.InProgress, project.Status);
+            Assert.NotNull(project.StartedAt);
+        }
+    }
+}
